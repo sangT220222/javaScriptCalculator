a69abee3f3bda64ade48a73f4bd76949a27322be
sangT220222 <magnas9@hotmail.co.uk>
2024-01-04 20:50:11 +0000

    basics of a calculator implemented

---
+++ jsCalculator.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
