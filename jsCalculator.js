21a058a5cb516df3cdbfb29f725ad5b7eaa2b1ec
sangT220222 <magnas9@hotmail.co.uk>
2024-01-04 22:38:18 +0000

    changed the layout of calculator to look like conventional one

--- jsCalculator.js
+++ jsCalculator.js
@@ -1,37 +1,34 @@
 //create 4 buttons with corresponding operations for user to click on
 
-const add_button = document.createElement("button");
-add_button.textContent = "+";
-add_button.setAttribute("id", "add_btn");
+// const add_button = document.createElement("button");
+// add_button.textContent = "+";
+// add_button.setAttribute("id", "add_btn");
 
-const subtract_button = document.createElement("button");
-subtract_button.textContent = "-";
-subtract_button.setAttribute("id", "sub_btn");
+// const subtract_button = document.createElement("button");
+// subtract_button.textContent = "-";
+// subtract_button.setAttribute("id", "sub_btn");
 
-const multiply_button = document.createElement("button");
-multiply_button.textContent = "*";
-multiply_button.setAttribute("id", "times_btn");
+// const multiply_button = document.createElement("button");
+// multiply_button.textContent = "*";
+// multiply_button.setAttribute("id", "times_btn");
 
-const divide_button = document.createElement("button");
-divide_button.textContent = "/";
-divide_button.setAttribute("id", "divide_btn");
+// const divide_button = document.createElement("button");
+// divide_button.textContent = "/";
+// divide_button.setAttribute("id", "divide_btn");
 
-//create input tag for user to enter numbers they like
+// //create input tag for user to enter numbers they like
 
-const input1 = document.createElement("input");
-input1.textContent = "Enter a number";
-input1.setAttribute("id", "input1");
+// const input1 = document.createElement("input");
+// input1.textContent = "Enter a number";
+// input1.setAttribute("id", "input1");
 
-const input2 = document.createElement("input");
-input2.textContent = "Enter another number";
-input2.setAttribute("id", "input2");
+// const input2 = document.createElement("input");
+// input2.textContent = "Enter another number";
+// input2.setAttribute("id", "input2");
 
-const display_box = document.createElement("div");
-display_box.textContent = "Result: 0";
-display_box.setAttribute("id", "result_box");
-
-
-
+// const display_box = document.createElement("div");
+// display_box.textContent = "Result: 0";
+// display_box.setAttribute("id", "result_box");
 
 const add = (num1, num2) => num1 + num2;
 
@@ -68,30 +65,22 @@ const operate = () => {
     }
 
 } 
-const main = () => {
-    divide_button.addEventListener("click", () => {
-        //add trial and error later
-        const num1 = parseFloat(input1.value);
-        const num2 = parseFloat(input2.value);
 
-        const result = divide(num1,num2);
-        display_box.textContent = `Result: ${result}`;
-    })
-}
+appendToDisplay = (value) => document.getElementById('display').value += value;
 
+clearDisplay = () => document.getElementById('display').value = '';
+  
 // console.log(add(2,3));
 // console.log(subtract(2,3));
 // console.log(multiply(2,3));
 // console.log(divide(2,3));
 
-document.body.append(input1);
-document.body.append(input2);
+// document.body.append(input1);
+// document.body.append(input2);
 
-document.body.append(add_button);
-document.body.append(subtract_button);
-document.body.append(multiply_button);
-document.body.append(divide_button);
+// document.body.append(add_button);
+// document.body.append(subtract_button);
+// document.body.append(multiply_button);
+// document.body.append(divide_button);
 
-document.body.append(display_box);
-
-main();
+// document.body.append(display_box);
