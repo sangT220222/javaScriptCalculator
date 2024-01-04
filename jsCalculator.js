f42d4000d5b0c02b8d05df2d15cb5655b90be398
sangT220222 <magnas9@hotmail.co.uk>
2024-01-04 21:05:20 +0000

    index file for browser interaction, html tags created through the use of DOM

--- jsCalculator.js
+++ jsCalculator.js
@@ -1,3 +1,15 @@
+//create 4 buttons with corresponding operations for user to click on
+
+const add_button = document.createElement("button");
+add_button.textContent = "+";
+const subtract_button = document.createElement("button");
+subtract_button.textContent = "-";
+const multiply_button = document.createElement("button");
+multiply_button.textContent = "*";
+const divide_button = document.createElement("button");
+divide_button.textContent = "/";
+
+
 const add = (num1, num2) => {
     return num1 + num2;
 }
@@ -14,7 +26,20 @@ const divide = (num1, num2) => {
     return num1 / num2;
 }
 
-console.log(add(2,3));
-console.log(subtract(2,3));
-console.log(multiply(2,3));
-console.log(divide(2,3));
+const main = () => {
+    
+}
+// A calculator operation will consist of a number, an operator, and another number.
+//  For example, 3 + 5. Create three variables for each of the parts of a calculator operation. 
+//  Create a variable for the first number, the operator, and the second number. 
+//  You’ll use these variables to update your display later.
+
+// console.log(add(2,3));
+// console.log(subtract(2,3));
+// console.log(multiply(2,3));
+// console.log(divide(2,3));
+
+document.body.append(add_button);
+document.body.append(subtract_button);
+document.body.append(multiply_button);
+document.body.append(divide_button);
