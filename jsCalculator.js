4cb1218e11029270b6187c87f7abb0265cfe84c4
sangT220222 <magnas9@hotmail.co.uk>
2024-01-06 19:24:56 +0000

    sorted logic out when equal is pressed, appropriate calculation is done

--- jsCalculator.js
+++ jsCalculator.js
@@ -4,11 +4,14 @@ const add_btn = document.getElementById("add_button");
 const subtract_btn = document.getElementById("subtract_button");
 const multiply_btn = document.getElementById("multiply_button");
 const divide_btn = document.getElementById("divide_button");
+const equal_btn = document.getElementById("equal_button");
+
+var result_to_display = null;
 
 // object to manage inputs
 const calculator = {
-    input1: '',
-    input2: '',
+    input1: "",
+    input2: "",
     operator: undefined,
   };
 
@@ -22,7 +25,7 @@ const divide = (num1, num2) => num1 / num2;
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
-const operate = () => {
+const operate = (num1,num2,operator) => {
     switch(operator){
         case "+":
             return add(num1,num2);
@@ -42,23 +45,31 @@ const operate = () => {
 // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 number_to_display = (number) =>
 {
-    //stores number clicked in increment so in 'input1' of object if operator isn't defined, else store in input2
-    calculator[calculator.operator ? 'input2' : 'input1'] += number;
+    //stores number clicked in increment so in "input1" of object if operator isn't defined, else store in input2
+    calculator[calculator.operator ? "input2" : "input1"] += number;
     //checks if operator is true, if it is display.value = input2.value, else input1.value
     console.log(calculator);
-    display.value = calculator[calculator.operator ? 'input2' : 'input1']
+    display.value = calculator[calculator.operator ? "input2" : "input1"]
 }
 
 //event listeners
 add_btn.addEventListener("click", function(){
-    calculator['operator'] = "+";
+    calculator["operator"] = "+";
 })
 subtract_btn.addEventListener("click", function(){
-    calculator['operator'] = "-";
+    calculator["operator"] = "-";
 })
 multiply_btn.addEventListener("click", function(){
-    calculator['operator'] = "*";
+    calculator["operator"] = "*";
 })
 divide_btn.addEventListener("click", function(){
-    calculator['operator'] = "/";
+    calculator["operator"] = "/";
+})
+
+equal_btn.addEventListener("click", function(){
+    var num1 = parseFloat(calculator["input1"]);
+    var num2 = parseFloat(calculator["input2"]);
+    var operator = calculator["operator"];
+    result_to_display = operate(num1,num2,operator);
+    console.log(result_to_display);
 })
