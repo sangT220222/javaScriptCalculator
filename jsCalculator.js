2192cd576b804c4121a0bbbcdfcea9954d220947
sangT220222 <magnas9@hotmail.co.uk>
2024-01-07 00:43:07 +0000

    tweaked logic so that calculator will be able to add and add when plus button is clicked repeatedley

--- jsCalculator.js
+++ jsCalculator.js
@@ -15,13 +15,13 @@ const calculator = {
     operator: undefined,
   };
 
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => parseFloat(num1) + parseFloat(num2);
 
-const subtract = (num1, num2) => num1 - num2;
+const subtract = (num1, num2) => parseFloat(num1) - parseFloat(num2);
 
-const multiply = (num1, num2) => num1 * num2;
+const multiply = (num1, num2) => parseFloat(num1) * parseFloat(num2);
 
-const divide = (num1, num2) => num1 / num2;
+const divide = (num1, num2) => parseFloat(num1) / parseFloat(num2);
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
@@ -40,36 +40,68 @@ const operate = (num1,num2,operator) => {
             return;
     }               
 } 
-
+const get_display_value = () => {
+    return document.getElementById("display").value;
+}
 // Create the functions that populate the display when you click the number buttons. 
 // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 number_to_display = (number) =>
 {
     //stores number clicked in increment so in "input1" of object if operator isn't defined, else store in input2
-    calculator[calculator.operator ? "input2" : "input1"] += number;
     //checks if operator is true, if it is display.value = input2.value, else input1.value
-    console.log(calculator);
+    calculator[calculator.operator ? "input2" : "input1"] += number.toString();
     display.value = calculator[calculator.operator ? "input2" : "input1"]
+    console.log(calculator);
 }
-
 //event listeners
 add_btn.addEventListener("click", function(){
-    calculator["operator"] = "+";
+    if(!calculator.operator){
+        calculator["operator"] = "+";
+    }
+    else if(typeof calculator.input1 === "string" && typeof calculator.input2 === "string" && typeof calculator.operator === "string"){
+        //do calculation here
+        // console.log(calculator);
+        result_to_display = operate(calculator["input1"],calculator["input2"],"+");
+        display.value = result_to_display;
+        // // console.log("TEST");
+        calculator["input1"] = get_display_value(); 
+        calculator["input2"] = "";
+        // calculator["operator"] = "+";
+    }
+    console.log(calculator);
+
+    // if(typeof calculator.input2 === 'number'){
+    //     console.log("YEs")
+    // }else{
+    //     console.log("NOPE");
+    // }
 })
 subtract_btn.addEventListener("click", function(){
+    if(!calculator.output){
+        calculator["operator"] = "-";
+    }
+    calculator["input1"] = get_display_value();
+    calculator["input2"] = "";
     calculator["operator"] = "-";
 })
 multiply_btn.addEventListener("click", function(){
+    if(!calculator.output){
+        calculator["operator"] = "*";
+    }
+    calculator["input1"] = get_display_value();
+    calculator["input2"] = "";
     calculator["operator"] = "*";
 })
 divide_btn.addEventListener("click", function(){
+    if(!calculator.output){
+        calculator["operator"] = "/";
+    }
+    calculator["input1"] = get_display_value();
+    calculator["input2"] = "";
     calculator["operator"] = "/";
 })
 
 equal_btn.addEventListener("click", function(){
-    var num1 = parseFloat(calculator["input1"]);
-    var num2 = parseFloat(calculator["input2"]);
-    var operator = calculator["operator"];
-    result_to_display = operate(num1,num2,operator);
-    console.log(result_to_display);
+    result_to_display = operate(calculator["input1"],calculator["input2"],calculator["operator"]);
+    display.value = result_to_display;
 })
